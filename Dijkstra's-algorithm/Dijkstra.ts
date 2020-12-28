313fa1124091ff5b7b6ced427ad751364141aff0
Artem Tumin <arttumin@gmail.com>
2020-12-28 16:31:47 +0300

    Update Dijkstra.ts

--- Dijkstra's-algorithm/Dijkstra.ts
+++ Dijkstra's-algorithm/Dijkstra.ts
@@ -1,30 +1,41 @@
-type Edge = {
-    vertex: string,
-    weight: number,
-};
+interface IEdge {
+    vertex: string;
+    weight: number;
+}
 
-type AdjacencyList = {
-    [key: string]: Edge[]
-};
-
-type PriorityQueueItem = {
-    val: string,
-    priority: number,
-};
-
-interface INaivePriorityQueue {
-    values: PriorityQueueItem[];
+interface IAdjacencyList {
+    [key: string]: IEdge[];
 }
 
 interface IWeightedGraph {
-    readonly adjacencyList: AdjacencyList;
+    readonly adjacencyList: IAdjacencyList;
+
+    addVertex(vertex: string): IWeightedGraph;
+    addEdge(vertexOne: string, vertexTwo: string, weight: number): IWeightedGraph;
+}
+
+interface IPriorityQueueItem {
+    value: string;
+    priority: number;
+}
+
+interface INaivePriorityQueue {
+    values: IPriorityQueueItem[];
+}
+
+interface IDijkstraDistances {
+    [key: string]: number;
+}
+
+interface IDijkstraPrevious {
+    [key: string]: string | null;
 }
 
 class NaivePriorityQueue implements INaivePriorityQueue {
-    values: PriorityQueueItem[] = [];
+    readonly values: IPriorityQueueItem[] = [];
 
-    enqueue(val: string, priority: number) {
-        this.values.push({ val, priority });
+    enqueue(value: string, priority: number) {
+        this.values.push({ value, priority });
         this.sort();
     }
 
@@ -32,18 +43,24 @@ class NaivePriorityQueue implements INaivePriorityQueue {
         return this.values.shift();
     }
 
-    sort() {
+    get length() {
+        return this.values.length;
+    }
+
+    private sort() {
         this.values.sort( (a, b) => a.priority - b.priority );
     }
 }
 
 class WeightedGraph implements IWeightedGraph {
-    adjacencyList: AdjacencyList = {};
+    adjacencyList: IAdjacencyList = {};
+
+    constructor() {}
 
     addVertex(vertex: string): IWeightedGraph {
         const vertexFound = this.adjacencyList[vertex];
 
-        if ( !vertexFound ) {
+        if (!vertexFound) {
             this.adjacencyList[vertex] = [];
         }
 
@@ -51,21 +68,81 @@ class WeightedGraph implements IWeightedGraph {
     }
 
     addEdge(vertexOne: string, vertexTwo: string, weight: number): IWeightedGraph {
-        this.adjacencyList[vertexOne].push({ vertex: vertexTwo, weight });
-        this.adjacencyList[vertexTwo].push({ vertex: vertexOne, weight });
-        
+        this.adjacencyList[vertexOne].push({vertex: vertexTwo, weight});
+        this.adjacencyList[vertexTwo].push({vertex: vertexOne, weight});
+
         return this;
+    }
+
+    execDijkstra(startingVertex: string, endingVertex: string) {
+        let path: string[] = [];
+        const verticesPriorityQueue: NaivePriorityQueue = new NaivePriorityQueue();
+        const previous: IDijkstraPrevious = {};
+        const distances: IDijkstraDistances = Object.keys(this.adjacencyList)
+            .reduce((acc: IDijkstraDistances, curr) => {
+                acc[curr] = curr === startingVertex
+                    ? 0
+                    : Infinity;
+
+                return acc;
+            }, {});
+
+        Object.entries(distances).forEach(([vertex, weight]) => {
+            verticesPriorityQueue.enqueue(vertex, weight);
+            previous[vertex] = null;
+        });
+
+        while(verticesPriorityQueue.length > 0) {
+            const { value: currentVertex, priority: currentVertexWeight } = verticesPriorityQueue.dequeue()!;
+
+            if (currentVertex === endingVertex) {
+                let currentStep = currentVertex;
+
+                if ( previous[endingVertex] === null ) {
+                    return [];
+                }
+
+                do {
+                    path.push(currentStep);
+                    currentStep = previous[currentStep];
+                } while (currentStep !== null);
+
+                path = path.reverse();
+
+                break;
+            }
+
+            this.adjacencyList[currentVertex].forEach(({ vertex: neighbourVertex, weight: neighbourWeight }) => {
+                const weightFromCurrentVertex = currentVertexWeight + neighbourWeight;
+
+                if (weightFromCurrentVertex < distances[neighbourVertex]) {
+                    distances[neighbourVertex] = weightFromCurrentVertex;
+                    previous[neighbourVertex] = currentVertex;
+                    verticesPriorityQueue.enqueue(neighbourVertex, weightFromCurrentVertex);
+                }
+            })
+        }
+
+        return path;
     }
 }
 
-const graph = new WeightedGraph();
+const weightedGraph = new WeightedGraph();
 
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
+weightedGraph
+    .addVertex('A')
+    .addVertex('B')
+    .addVertex('C')
+    .addVertex('D')
+    .addVertex('E')
+    .addVertex('F')
+    .addEdge('A', 'B', 4)
+    .addEdge('A', 'C', 2)
+    .addEdge('B', 'E', 3)
+    .addEdge('C', 'D', 2)
+    .addEdge('C', 'F', 4)
+    .addEdge('D', 'E', 3)
+    .addEdge('D', 'F', 1)
+    .addEdge('E', 'F', 1);
 
-graph.addEdge("A", "B", 9);
-graph.addEdge("A", "C", 5);
-graph.addEdge("B", "C", 7);
-
-console.log( graph.adjacencyList );
+weightedGraph.execDijkstra('A', 'E');
